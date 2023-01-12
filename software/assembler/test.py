9f9e9e98376f22a2e0913e18bfac7904614b9160
Paul Bartel <Paul-Bartel@gmx.net>
2023-01-12 14:35:26 +0100

    connected parser.py and test.py

--- software/assembler/test.py
+++ software/assembler/test.py
@@ -1,4 +1,6 @@
 import re
+from lark import Lark, Token
+import asm_parser
 
 def tcomplement12(a: int):
   if(a < 0):
@@ -118,7 +120,50 @@ opDictionary = {
   'LUI': ['imm20','rd','1101111'],
   'AUIPC': ['imm20','rd','1101111'],
   }
-file = open("software/assembler/test.txt", "r").read().splitlines();
+registers = {
+  'x0': 0,
+  'zero': 0,
+  'x1': 1,
+  'ra': 1,
+  'x2': 2,
+  'x2': 2,
+  'x3': 3,
+  'gp': 3,
+  'x4': 4,
+  'tp': 4,
+  'x5': 5,
+  't0': 5,
+  'x6': 6,
+  'x7': 7,
+  'x8': 8,
+  'x9': 9,
+  'x10': 10,
+  'x11': 11,
+  'x12': 12,
+  'x13': 13,
+  'x14': 14,
+  'x15': 15,
+  'x16': 16,
+  'x17': 17,
+  'x18': 18,
+  'x19': 19,
+  'x20': 20,
+  'x21': 21,
+  'x22': 22,
+  'x23': 23,
+  'x24': 24,
+  'x25': 25,
+  'x26': 26,
+  'x27': 27,
+  'x28': 28,
+  'x29': 29,
+  'x30': 30,
+  'x31': 31,
+}  
+file = open("software/assembler/test.txt", "r");
+tree = asm_parser.createTree(file)
+for token in tree.scan_values(lambda v: isinstance(v, Token)):
+  print(token.value)
 memonic = ''
 program = ''
 count = 0
