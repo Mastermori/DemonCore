0b410e30460c89db8bdd28a21daa2143fc7da899
Mastermori <mastermoriii@gmail.com>
2023-01-28 05:30:05 +0100

    added completion, hover, signatures, goto-def, find-ref, diagnostics to ls

--- vs-extension/demonass/lsp-server/server.py
+++ vs-extension/demonass/lsp-server/server.py
@@ -1,51 +1,214 @@
+from json import JSONDecodeError
+import os
 import re as re
+import sys
+from typing import Dict, List
+from lark import UnexpectedInput
 from pygls.server import LanguageServer
 import lsprotocol.types as types
-from lsprotocol.types import (CompletionItem, CompletionList, CompletionOptions,
-                              CompletionParams, HoverParams, Hover)
+from lsprotocol.types import (CompletionList, CompletionOptions, CompletionParams, SignatureHelpOptions,
+                              SignatureHelpParams, HoverParams, SignatureHelp, TypeDefinitionParams, ReferenceParams)
 from urllib.parse import urlparse, unquote
+from server_doc import AssemblerDoc
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(
+    SCRIPT_DIR + "/../../../software/assembler/parser/"))
+from asm_parser import parse_with_context
 
 default_host = "127.0.0.1"
 default_port = 8080
 
 server = LanguageServer('DemonAssembler-Server', 'v0.1')
 
-def main(): 
+documents: Dict[str, List[str]] = {}
+
+
+def main():
+    print("Server started!")
     server.start_tcp(default_host, default_port)
+
+
+doc = AssemblerDoc("lsp-server/instruction_doc.txt")
+
 
 @server.feature(types.TEXT_DOCUMENT_COMPLETION, CompletionOptions(trigger_characters=[',']))
 def completions(params: CompletionParams):
     """Returns completion items."""
-    print(params)
+    line = get_file_contents(params.text_document.uri)[params.position.line]
+    line_to_cursor = line[:params.position.character]
+    split_line = line_to_cursor.split(" ")
+    items = []
+    if len(split_line) > 1:
+        probable_mnemonic = split_line[0]
+        param_index = line_to_cursor.count(",") + line_to_cursor.count("(")
+        param_completions = doc.get_param_completion_for_instruction(
+            probable_mnemonic, param_index)
+        items.extend(param_completions)
+    elif len(split_line) == 1:
+        items.extend(doc.get_instruction_completions())
     return CompletionList(
         is_incomplete=False,
-        items=[
-            CompletionItem(label='Item1'),
-            CompletionItem(label='Item2'),
-            CompletionItem(label='Item3'),
-        ]
+        items=items
     )
+
 
 @server.feature(types.TEXT_DOCUMENT_HOVER)
 def handle_hover(params: HoverParams):
-    file_path = unquote(urlparse(params.text_document.uri).path)
-    print(file_path[1:])
-    edited_file = open(file_path[1:])
-    lines = edited_file.read().split("\n")
+    lines = get_file_contents(params.text_document.uri)
     line = params.position.line
     character = params.position.character
-    word = find_word(lines[line], character)
-    print(word)
-    edited_file.close()
-    return Hover(contents=f"Line: {line}, Character: {character}, Word: {word}")
+    word, range = find_word(lines[line], character)
+    hover = doc.get_hover_for(word)
+    if hover:
+        return hover
+    # , range=types.Range())
+    # return Hover(contents=f"Line: {line}, Character: {character}, Word: {word}")
+
+
+@server.feature(types.TEXT_DOCUMENT_SIGNATURE_HELP, SignatureHelpOptions(trigger_characters=[',', '(']))
+def signature_help(params: SignatureHelpParams):
+    line = get_file_contents(params.text_document.uri)[params.position.line]
+    line_to_cursor = line[:params.position.character]
+    split_line = line_to_cursor.split(" ")
+    param_index = line_to_cursor.count(",") + line_to_cursor.count("(")
+    signature_information = doc.get_signature_info_for_instruction(
+        split_line[0])
+    if signature_information:
+        return SignatureHelp([signature_information], active_parameter=param_index)
+
+
+@server.feature(types.TEXT_DOCUMENT_DEFINITION)
+def goto_definition(params: TypeDefinitionParams):
+    lines = get_file_contents(params.text_document.uri)
+    position = params.position
+    word, range = find_word_from_position(lines, position)
+    if len(word.strip()) < 1:
+        return None
+    for line_index, line in enumerate(lines):
+        found_index = line.find(word)
+        if found_index < 0:
+            continue
+        end_index = found_index + range[1] - range[0]
+        if lines[line_index][end_index+1] == ":":
+            return types.Location(params.text_document.uri, types.Range(types.Position(line_index, found_index), types.Position(line_index, end_index)))
+
+
+@server.feature(types.TEXT_DOCUMENT_REFERENCES)
+def find_references(params: ReferenceParams):
+    lines = get_file_contents(params.text_document.uri)
+    position = params.position
+    word, range = find_word_from_position(lines, position)
+    if len(word.strip()) < 1:
+        return None
+    found_locations = []
+    for line_index, line in enumerate(lines):
+        found_index = line.find(word)
+        if found_index < 0:
+            continue
+        end_index = found_index + range[1] - range[0]
+        found_locations.append(types.Location(params.text_document.uri, types.Range(
+            types.Position(line_index, found_index), types.Position(line_index, end_index))))
+    return found_locations
+
+
+@server.feature(types.TEXT_DOCUMENT_DID_CHANGE)
+def did_change(params: types.DidChangeTextDocumentParams):
+    """Text document did change notification."""
+    _validate(params)
+
+
+@server.feature(types.TEXT_DOCUMENT_DID_CLOSE)
+def did_close(params: types.DidCloseTextDocumentParams):
+    """Text document did close notification."""
+    server.show_message('Text Document Did Close')
+
+
+@server.feature(types.TEXT_DOCUMENT_DID_OPEN)
+async def did_open(params: types.DidOpenTextDocumentParams):
+    """Text document did open notification."""
+    server.show_message('Text Document Did Open')
+    _validate(params)
+
+
+def _validate(params):
+    server.show_message_log('Validating assembler code...')
+
+    text_doc = server.workspace.get_document(params.text_document.uri)
+
+    source = text_doc.source
+    diagnostics = _validate_assembler(source) if source else []
+
+    server.publish_diagnostics(text_doc.uri, diagnostics)
+
+
+def _validate_assembler(source: str):
+    diagnostics = []
+
+    try:
+        parse_context = parse_with_context(source)
+        for error in parse_context.get_errors():
+            msg = error.msg
+            col = error.colno
+            line = error.lineno
+
+            d = types.Diagnostic(
+                range=types.Range(
+                    start=types.Position(line=line - 1, character=col - 1),
+                    end=types.Position(line=line - 1, character=col)
+                ),
+                message=msg,
+                source=type(server).__name__
+            )
+            diagnostics.append(d)
+        # diagnostics.extend(parse_context.get_errors())
+    except UnexpectedInput as err:
+        # msg = err.msg
+        msg = f"There is an {type(err).__name__} here."
+        print(err.args)
+        col = err.column
+        line = err.line
+
+        d = types.Diagnostic(
+            range=types.Range(
+                start=types.Position(line=line - 1, character=col - 1),
+                end=types.Position(line=line - 1, character=col)
+            ),
+            message=msg,
+            source=type(server).__name__
+        )
+
+        diagnostics.append(d)
+        print("Error occured during diagnostics")
+
+    return diagnostics
+
+
+def read_file_contents(uri: str) -> List[str]:
+    file_path = unquote(urlparse(uri).path)
+    with open(file_path[1:]) as edited_file:
+        return edited_file.read().split("\n")
+
+
+def get_file_contents(uri: str) -> List[str]:
+    return server.workspace.get_document(uri).lines
+
 
 def find_word(line: str, position: int) -> str:
-    word_indices = [(ele.start(), ele.end() - 1) for ele in re.finditer(r'\w+|\d+', line)]
+    line = line.removesuffix("\n").removesuffix("\r")
+    if position == len(line):
+        return find_word(line, position - 1)
+    word_indices = [(ele.start(), ele.end() - 1)
+                    for ele in re.finditer(r'\w+|\d+', line)]
     for i in word_indices:
         if i[0] <= position and i[1] >= position:
-            print(i)
-            return line[i[0]:i[1]+1]
-    return line[position]
+            return line[i[0]:i[1]+1], i
+    return line[position], (position, position)
+
+
+def find_word_from_position(file_lines: List[str], position: types.Position) -> str:
+    return find_word(file_lines[position.line], position.character)
+
 
 if __name__ == "__main__":
     main()
---
+++ vs-extension/demonass/lsp-server/server_doc.py
@@ -0,0 +1,289 @@
+import abc
+import re as re
+import dictionaries
+import lsprotocol.types as types
+from dataclasses import dataclass
+from typing import Dict, List
+from lsprotocol.types import (CompletionItem, Hover, SignatureInformation, ParameterInformation)
+
+
+@dataclass
+class ParamDefinition(abc.ABC):
+    name: str
+    description: str
+
+    @abc.abstractmethod
+    def get_completion(self) -> List[CompletionItem]:
+        pass
+    
+    def get_param_info(self) -> ParameterInformation:
+        return ParameterInformation(self.name, self.description)
+
+
+class RegisterParamDefinition(ParamDefinition):
+
+    def __init__(self, name: str, description: str):
+        super().__init__(name, description)
+
+    def get_completion(self) -> List[CompletionItem]:
+        items = []
+        for register in dictionaries.registers:
+            items.append(CompletionItem(
+                label=register, kind=types.CompletionItemKind.Variable, detail=dictionaries.registers[register]))
+        return items
+
+
+# TODO: implement for %hi and %lo
+class ImmediateParamDefinition(ParamDefinition):
+    def __init__(self, name: str, description: str):
+        super().__init__(name, description)
+
+    def get_completion(self) -> List[CompletionItem]:
+        return []
+
+
+class InstructionDefinition(abc.ABC):
+    indentation_level = 10
+
+    name: str
+    type: str
+    param_definitions: List[ParamDefinition]
+    documentation: types.MarkupContent
+    description: str
+    deprecated = False
+
+    def __init__(self, name: str, type: str, param_definitions: List[ParamDefinition], description: str = "") -> None:
+        self.name = name
+        self.type = type
+        self.param_definitions = param_definitions
+        self.set_description(description)
+
+    def get_param_definition(self, param_index: int) -> ParamDefinition:
+        if not self.param_definitions:
+            return None
+        if param_index >= len(self.param_definitions):
+            return None
+        return self.param_definitions[param_index]
+
+    def get_param_completion(self, param_index) -> List[CompletionItem]:
+        if param_index >= len(self.param_definitions):
+            return []
+        return self.param_definitions[param_index].get_completion()
+
+    def get_completion(self) -> CompletionItem:
+        name = self.name
+        indentation = ' ' * \
+            (InstructionDefinition.indentation_level - len(name))
+        insert_text = self.get_insert_text(indentation)
+        return CompletionItem(label=name, insert_text=insert_text,
+                              kind=types.CompletionItemKind.Method, insert_text_format=types.InsertTextFormat.Snippet,
+                              documentation=self.get_description(), command=types.Command("", "editor.action.triggerParameterHints"),
+                              tags=[types.CompletionItemTag.Deprecated] if self.deprecated else None, sort_text="zzz" if self.deprecated else None)
+
+    def get_hover(self) -> Hover:
+        return Hover(contents=self.get_description())
+
+    def get_signature_info(self) -> SignatureInformation:
+        parameters = []
+        for param in self.param_definitions:
+            parameters.append(param.get_param_info())
+        return SignatureInformation(label=self.get_signature_label(), documentation=self.description, parameters=parameters)
+
+    def get_signature_label(self) -> str:
+        return re.sub(r"\${\d+:(\w+)}", r"\1", self.get_insert_text())
+
+    def _set_markdown_documentation(self) -> None:
+        lines = []
+        if self.deprecated:
+            lines.append("## Deprecated  \n")
+        lines.extend([
+            '**' + self.get_signature_label() + '**',
+            '\n---\n',
+        ])
+        lines.extend(self.description.split("\n"))
+        lines.append("\nParameters:  ")
+        for param_def in self.param_definitions:
+            indentation = "&nbsp;" * (10 - len(param_def.name))
+            lines.append(
+                f"&nbsp;&nbsp;&nbsp;&nbsp;{param_def.name}:{indentation}{param_def.description}  ")
+        self.documentation = types.MarkupContent(
+            types.MarkupKind.Markdown,
+            '\n'.join(lines)
+        )
+
+    def set_description(self, description: str) -> None:
+        self.description = description
+        self._set_markdown_documentation()
+
+    def get_description(self) -> types.MarkupContent:
+        return self.documentation
+
+    def set_deprecated(self, deprecated: bool) -> None:
+        self.deprecated = deprecated
+        self._set_markdown_documentation()
+    
+    def get_snippet(self, index: int) -> str:
+        return f"${{{index+1}:{self.param_definitions[index].name}}}"
+
+    @abc.abstractmethod
+    def get_insert_text(self, indentation=" ") -> str:
+        pass
+
+
+class RegisterInstructionDefinition(InstructionDefinition):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, "register", [
+            RegisterParamDefinition("rd", "Destination register"),
+            RegisterParamDefinition("rs1", "First operation register"),
+            RegisterParamDefinition("rs2", "Second operation register")
+        ], "Computes *rs1* and *rs2* into *rd*.")
+
+    def get_insert_text(self, indentation=" ") -> str:
+        return f"{self.name}{indentation}{self.get_snippet(0)}, {self.get_snippet(1)}, {self.get_snippet(2)}"
+
+
+class ImmediateInstructionDefinition(InstructionDefinition):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, "immediate", [
+            RegisterParamDefinition("rd", "Destination register"),
+            RegisterParamDefinition("rs1", "First operation register"),
+            ImmediateParamDefinition("imm12", "12-bit immediate value")
+        ], "Computes *rs1* and *imm12* to *rd*.")
+
+    def get_insert_text(self, indentation=" ") -> str:
+        return f"{self.name}{indentation}{self.get_snippet(0)}, {self.get_snippet(1)}, {self.get_snippet(2)}"
+
+
+class ImmediateshamtInstructionDefinition(InstructionDefinition):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, "immediate", [
+            RegisterParamDefinition("rd", "Destination register"),
+            RegisterParamDefinition("rs1", "Register to shift"),
+            ImmediateParamDefinition(
+                "shamt", "5-bit immediate value to shift with")
+        ], "Shifts *rs1* by *shamt* and writes the result to *rd*.")
+
+    def get_insert_text(self, indentation=" ") -> str:
+        return f"{self.name}{indentation}{self.get_snippet(0)}, {self.get_snippet(1)}, {self.get_snippet(2)}"
+
+
+class LoadInstructionDefinition(InstructionDefinition):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, "load", [
+            RegisterParamDefinition("rs1", "Register that will be loaded to"),
+            ImmediateParamDefinition(
+                "offset", "12-bit immediate offset added to *rs2*"),
+            RegisterParamDefinition(
+                "rs2", "Memory address to load from (+ *offset*)")
+        ], "Loads memory at address *rs2* + *offset* and writes the value to *rs1*.")
+
+    def get_insert_text(self, indentation=" ") -> str:
+        return f"{self.name}{indentation}{self.get_snippet(0)}, {self.get_snippet(1)}({self.get_snippet(2)})"
+
+
+class SaveInstructionDefinition(InstructionDefinition):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, "save", [
+            RegisterParamDefinition("rs1", "Register that will be saved"),
+            ImmediateParamDefinition(
+                "offset", "12-bit immediate offset added to *rs2*"),
+            RegisterParamDefinition(
+                "rs2", "Memory address to save *rs1* to (+ *offset*)")
+        ], "Saves *rs1* to memory at address *rs2* + *offset*.")
+
+    def get_insert_text(self, indentation=" ") -> str:
+        return f"{self.name}{indentation}{self.get_snippet(0)}, {self.get_snippet(1)}({self.get_snippet(2)})"
+
+
+class BranchInstructionDefinition(InstructionDefinition):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, "branch", [
+            RegisterParamDefinition("rs1", "First register to compare"),
+            RegisterParamDefinition("rs2", "Second register to compare"),
+            ImmediateParamDefinition(
+                "offset", "12-bit immediate offset that will be added to pc")
+        ], "Branches (adds *offset* to pc) if the condition with *rs1* and *rs2* is fullfilled.")
+
+    def get_insert_text(self, indentation=" ") -> str:
+        return f"{self.name}{indentation}{self.get_snippet(0)}, {self.get_snippet(1)}, {self.get_snippet(2)}"
+
+
+class BigimmediateInstructionDefinition(InstructionDefinition):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, "bigimmediate", [
+            RegisterParamDefinition("rd", "Destination register"),
+            ImmediateParamDefinition("imm20", "Immediate value to compute")
+        ], "Computes *imm20* and saves result to *rd*.")
+
+    def get_insert_text(self, indentation=" ") -> str:
+        return f"{self.name}{indentation}{self.get_snippet(0)}, {self.get_snippet(1)}"
+
+
+class ParseContext:
+    current_type: str
+    current_instruction: InstructionDefinition
+
+
+class AssemblerDoc:
+    instructions: Dict[str, InstructionDefinition] = {}
+
+    def __init__(self, file_path: str):
+        self.instructions = self.parse(file_path)
+
+    def parse(self, file_path: str):
+        context = ParseContext()
+        instructions = {}
+        with open(file_path, "r") as file_content:
+            for line in file_content:
+                stripped = line.strip()
+                if stripped.startswith("@"):
+                    self.parse_annotation(line[1:], context)
+                elif len(stripped) > 0:
+                    name = stripped
+                    instructions[name] = globals(
+                    )[f"{context.current_type.capitalize()}InstructionDefinition"](name)
+                    context.current_instruction = instructions[name]
+        return instructions
+
+    def parse_annotation(self, line: str, context: ParseContext):
+        split_line = line.split(" ")
+        annotation_name = split_line[0].strip()
+        match annotation_name:
+            case "type":
+                type = split_line[1].strip()
+                context.current_type = type
+            case "param":
+                name = split_line[1].strip()
+                description = " ".join(split_line[2:]).removesuffix("\n")
+                for param_definition in context.current_instruction.param_definitions:
+                    if param_definition.name == name:
+                        param_definition.description = description
+            case "description":
+                description = " ".join(split_line[1:]).removesuffix(
+                    "\n").replace("\\n", "\n")
+                context.current_instruction.set_description(description)
+            case "deprecated":
+                context.current_instruction.set_deprecated(True)
+
+    def get_instruction_completions(self) -> List[CompletionItem]:
+        return [definition.get_completion() for definition in self.instructions.values()]
+
+    def get_param_completion_for_instruction(self, instruction_name: str, param_index: int) -> List[CompletionItem]:
+        if instruction_name in self.instructions:
+            instruction_definition = self.instructions[instruction_name]
+            return instruction_definition.get_param_completion(param_index)
+        return []
+
+    def get_hover_for(self, name: str) -> Hover:
+        if name in self.instructions:
+            instruction_definition = self.instructions[name]
+            return instruction_definition.get_hover()
+        if name in dictionaries.registers:
+            return Hover(contents=dictionaries.registers[name])
+        return None
+
+    def get_signature_info_for_instruction(self, instruction_name: str) -> SignatureInformation:
+        if instruction_name in self.instructions:
+            instruction_definition = self.instructions[instruction_name]
+            return instruction_definition.get_signature_info()
+        return None
